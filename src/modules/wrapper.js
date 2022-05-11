133861053f77c931ce15c2bb873ca2898ecc8d98
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-11 20:20:55 +0200

    conform the wrapper to the current data

--- src/modules/wrapper.js
+++ src/modules/wrapper.js
@@ -2,9 +2,9 @@ const Wrapper = () => {
   document.body.innerHTML = `<section class="container">
     <h1>Leaderboard</h1>
     <section class="wrapper">
-    <section class="output-section"><div class="content-header"><h2>Recent Scores</h2><button class="refresh">Refresh</button></div><div id="content" class="content"></div></h2></section>
-    <section class="input-section"><h2 class="add">Add your score</h2><input type="text" placeholder="your name">
-    <input type="text" placeholder="your score"><button type="submit" class="refresh submit">submit</button></section>
+    <section class="output-section"><div class="content-header"><h2>Recent Scores</h2><button class="refresh" id="refresh">Refresh</button></div><div id="content" class="content"></div></h2></section>
+    <section class="input-section"><h2 class="add">Add your score</h2><input type="text" placeholder="your name" class="name">
+    <input type="text" placeholder="your score" class="score"><button type="submit" class="refresh submit">submit</button></section>
     </section></section>`;
 };
 
