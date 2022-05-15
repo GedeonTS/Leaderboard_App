fadf46ef6a5e7288f13b7ed07ec9035ddc2b3d6d
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-15 13:28:07 +0200

    Updates

--- runtime.main.js
+++ runtime.main.js
@@ -104,7 +104,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/webpack-demo/";
+/******/ 		__webpack_require__.p = "";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
